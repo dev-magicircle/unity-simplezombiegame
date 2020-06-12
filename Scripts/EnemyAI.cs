1e256747ade6e0a702ca752760ebd9dbad0b22c9
dev-magicircle <62414035+dev-magicircle@users.noreply.github.com>
2020-06-13 02:58:40 +0900

    Add files via upload

---
+++ Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;//추가
+
+public class EnemyAI : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player=GameObject.FindGameObjectWithTag("Player");//플레이어 태그 찾음
+        //플레이어가 없으면
+        if(player==null)
+        {
+          Debug.Log("플레이어가 없음");
+        }
+        enemy= GetComponent<NavMeshAgent>();//좀비
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.destination=player.transform.position;//좀비가 플레이어를 따라감.
+    }
+}
---
+++ Scripts/ShowUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+  public GameObject uiObject;//화면 
+  
+    //시작할 때는 비활성화
+    void Start()
+    {
+      Debug.Log("start");
+        uiObject.SetActive(false);
+    }
+    //player=GameObject.FindGameObjectWithTag("Player");
+    //플레이어와 만났을 때 이벤트 시작
+    void OnTriggerEnter(Collider player)
+    {
+      if(player.gameObject.tag=="Player")
+      {
+        Debug.Log("meet"); 
+        uiObject.SetActive(true);
+        StartCoroutine("WaitForSec");
+      }
+    }
+
+    //UI화면없앰
+    IEnumerator WaitForSec()
+    {
+      yield return new WaitForSeconds(5);//5초대기
+      Destroy(uiObject);
+      Destroy(gameObject);
+    }
+}
